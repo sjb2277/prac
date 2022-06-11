79c6b5a5e27a8a7381b381ed8b9f9ea711464d51
sjb2277 <lol2277@naver.com>
2022-06-11 23:03:00 +0900

    first commit

---
+++ animal.java
@@ -0,0 +1,11 @@
+class dog(int x, int y){
+    public static void sound() {
+        System.out.println("월월");
+    }
+
+}
+public class animal {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
