e9c6106cd5ebd7d1bea321d61b43258a4d22bcda
sjb2277 <lol2277@naver.com>
2022-06-11 23:06:49 +0900

    first self practice

--- asd.java
+++ asd.java
@@ -1,5 +1,9 @@
 public class asd{
     public static void main(String[] args) {
         System.out.println("Hello world");
+        int x=0;
+        for(int i=0;i<10;i++){
+            System.out.println(i+"번째");
+        }
     }
 }
